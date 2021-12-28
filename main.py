9e2b6cd56383549d2c02b93fffa7ffd2d6ee21ba
thale01veloso <thale01veloso@gmail.com>
2021-12-28 09:03:55 -0300

    Adiciona acao ao botao cadastrar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from connect import atualizarPessoa, deletarPessoa, inserir, mostrarTabelaTodos,
 from model import Pessoa
 from flask import Flask, render_template
 import pandas as pd
+from flask.globals import request
 
 app = Flask(__name__)
 @app.route('/')
@@ -9,6 +10,15 @@ def index():
     result = mostrarTodos()
     return render_template("index.html",
     result = result)
+
+@app.route("/cadastrar", methods=["POST","GET"])
+def cadastrar():
+    nome = request.form["nome"]
+    cpf = request.form["cpf"]
+    dataNascimento = request.form["dataNascimento"]
+    pessoa = Pessoa(cpf,nome,dataNascimento)
+    inserir(pessoa)
+    return render_template("index.html")
 if __name__=="__main__":
     app.run(debug=True)
 
